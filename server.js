f93aa8e78b7b1a1d1fe1730d919e565f61693c93
andre <andremorais0000@gmail.com>
2024-09-14 18:56:05 -0300

    Added get and post routes

--- server.js
+++ server.js
@@ -2,4 +2,16 @@ import express from 'express'
 
 const app = express()
 
+app.get('/user', (request, response) => {
+    response.send("Hello world, route accessed successfully")
+})
+
+app.post('/user', (req, res) => {
+    res.send("Hello my friend")
+})
+
 app.listen(3000)
+
+// req - request ou requisição
+// res - response ou resposta
+// http://localhost:3000
