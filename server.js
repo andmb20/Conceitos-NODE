9de049574f949f8d6de24ddfedccb7ff0e00e702
andre <andremorais0000@gmail.com>
2024-09-14 22:42:54 -0300

    creating a registration

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const users = []
 
 app.get('/user', (request, response) => {
-    response.send("Hello world, route accessed successfully")
+    response.status(200).json(users)
 })
 
 app.post('/user', (req, res) => {
-    res.send("Hello my friend")
+    users.push(req.body)
+    res.status(201).json({message: "Usuário criado com sucesso"})
 })
 
 app.listen(3000)
