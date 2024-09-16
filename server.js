df1dd9f239aa06ba57289abe5472bb223aaecc91
andre <andremorais0000@gmail.com>
2024-09-15 21:15:19 -0300

    NodeJS concepts finalized

--- server.js
+++ server.js
@@ -1,17 +1,55 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
+app.get('/user', async (request, response) => {
 
-app.get('/user', (request, response) => {
+    const users = await prisma.user.findMany()
+
     response.status(200).json(users)
 })
 
-app.post('/user', (req, res) => {
-    users.push(req.body)
-    res.status(201).json({message: "Usuário criado com sucesso"})
+app.post('/user', async (req, res) => {
+
+    const usuario = await prisma.user.create({
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+
+    res.status(201).json(usuario)
+})
+
+app.put('/user/:id', async (req, res) => {
+
+    const usuario = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email
+        }
+    })
+
+    res.status(200).json(usuario)
+})
+
+app.delete('/user/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+
+    })
+    res.status(200).json({ message: "Usuário deletado com sucesso" })
 })
 
 app.listen(3000)
